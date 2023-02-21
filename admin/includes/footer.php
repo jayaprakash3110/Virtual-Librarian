fc083f7c890b7b9bee44ee13906d4d2e0d413f37
JAYAPRAKASH P <105505086+jayaprakash3110@users.noreply.github.com>
2023-02-21 12:22:30 +0530

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; <?php echo date('Y');?> Virtual Librarian |<a href="https://github.com/jayaprakash3110" target="_blank"  > Designed by : JAYAPRAKASH P</a> 
+                   &copy; <?php echo date('Y');?> Virtual Librarian |<a href="https://github.com/jayaprakash3110/library" target="_blank"  > Designed by : JAYAPRAKASH P</a> 
                 </div>
 
             </div>
